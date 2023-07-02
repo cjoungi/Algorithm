ab80853a4cf9413a856506e7fa510cbac13f0972
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-02 16:35:55 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        //int형 변수 A와 B에 숫자 입력받기
         int A = sc.nextInt();
         int B = sc.nextInt();
         
