19607c3b1f86d35cd5fa1f72fc49634d6408d466
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-10 20:37:06 +0900

    [Bronze V] Title: 사분면 고르기, Time: 128 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,19 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        
-        String str = "";
-        
-        if(A>0 && B>0) str="1";
-        if(A<0 && B>0) str="2";
-        if(A<0 && B<0) str="3";
-        if(A>0 && B<0) str="4";
-        
-        System.out.println(str);
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println((A>0&&B>0)?"1":(A<0&&B>0)?"2":(A<0&&B<0)?"3":"4");
     }
 }
