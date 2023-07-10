c20c54c84557a65b3c85c171f76c20edf236e429
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-11 01:18:35 +0900

    [Bronze III] Title: 오븐 시계, Time: 156 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,20 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int hour = sc.nextInt();
-        int min = sc.nextInt();
-        int cook = sc.nextInt();
+        int h = Integer.parseInt(st.nextToken()); // 현재 시
+        int m = Integer.parseInt(st.nextToken()); // 현재 분
+        int t = Integer.parseInt(br.readLine()); // 요리 시간(분)
+
+        int finH = (h+(m+t)/60)%24; //요리 끝나는 시각
+        int finM = ((m+t)%60); //요리 끝나는 분
         
-        int finishedHour = (hour*60+min+cook)/60; //요리 끝나는 시각
-        int finishedMin = (hour*60+min+cook)%60; //요리 끝나는 분
-        
-        if(finishedHour>=24) { //24시가 지남
-            System.out.println(finishedHour-24+" "+finishedMin);
-        }
-        else System.out.println(finishedHour+" "+finishedMin);
-        
+        System.out.println(finH+" "+finM);
+     
     }
 }
