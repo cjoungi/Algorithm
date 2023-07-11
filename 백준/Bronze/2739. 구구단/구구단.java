98fedfd6f0f6319d97d8b98d978e2d168e0e31bd
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-11 19:05:23 +0900

    [Bronze V] Title: 구구단, Time: 152 ms, Memory: 16240 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int A = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= 9; i++) {
+			bw.write(A + " * " + i + " = " + A*i);
+            bw.newLine();
+		}
         
-        int A = sc.nextInt();
-        
-        for(int i=1;i<=9;i++){
-            System.out.println(A + " * " + i + " = " +A*i);
-        }
-    }
+        bw.flush();
+        bw.close();
+	}
+
 }
