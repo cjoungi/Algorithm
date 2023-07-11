0dd5ff870b042af1ca76323c79eafaa50511e997
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-11 17:23:46 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        
+        int A = Integer.parseInt(st.nextToken()); 
+        int B = Integer.parseInt(st.nextToken()); 
+        int C = Integer.parseInt(st.nextToken()); 
+       
         if(A==B && A==C && B==C) System.out.println(10000+A*1000);
         else if(A==B || A==C) System.out.println(1000+A*100);
         else if(B==C) System.out.println(1000+B*100);
