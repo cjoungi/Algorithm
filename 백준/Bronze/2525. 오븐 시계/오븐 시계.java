2ca891b39e93912ac519da0dbf8b6979b0d2dd6c
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-11 11:26:42 +0900

    [Bronze III] Title: 오븐 시계, Time: 152 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int h = Integer.parseInt(st.nextToken()); // 현재 시
-        int m = Integer.parseInt(st.nextToken()); // 현재 분
+        String[] arr = br.readLine().split(" "); 
+
+        int h = Integer.parseInt(arr[0]); // 현재 시
+        int m = Integer.parseInt(arr[1]); // 현재 분
         int t = Integer.parseInt(br.readLine()); // 요리 시간(분)
 
-        int finH = h+(m+t)/60; //요리 끝나는 시각
-        int finM = (m+t)%60; //요리 끝나는 분
+        int finH = (h+(m+t)/60)%24; //요리 끝나는 시각
+        int finM = ((m+t)%60); //요리 끝나는 분
         
-        System.out.println(finH>=24? finH-24+" "+finM : finH+" "+finM);
+        System.out.println(finH+" "+finM);
      
     }
 }
