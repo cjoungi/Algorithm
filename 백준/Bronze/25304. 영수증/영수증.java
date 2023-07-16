a610b7d3428fa0f7def0feeb803d011b1fcfda1e
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-07-16 15:33:03 +0900

    [Bronze V] Title: 영수증, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int X = sc.nextInt();
-        int N = sc.nextInt();
+        int X = Integer.parseInt(br.readLine()); 
+        int N = Integer.parseInt(br.readLine()); 
+        
         int sum = 0;
         
         for(int i=0;i<N;i++){ //구매한 물건 종류의 수만큼 반복
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+        
+            int a = Integer.parseInt(st.nextToken()); 
+            int b = Integer.parseInt(st.nextToken()); 
             sum += a*b;
         }
-        
+
         if(X==sum) System.out.println("Yes");
         else System.out.println("No");
         
