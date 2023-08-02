46b75991b1e64c03067b79813dd346c1e52ec89d
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-02 12:43:46 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,28 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
  
 public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
         
 		int num = Integer.parseInt(br.readLine());
  
+        StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
         
 		for (int i = 1; i <= num; i++) {
 			st = new StringTokenizer(br.readLine()," ");
             
-            bw.write("Case #" + i + ": ");
-            bw.write(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+"\n");
+            sb.append("Case #").append(i).append(": ");
+            sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())).append("\n");
 		}
 		br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
 	}
  
 }
