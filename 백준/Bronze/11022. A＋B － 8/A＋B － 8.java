9d9dd88350cd5c75c543f670cebc250c5a434e39
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-02 15:20:24 +0900

    [Bronze V] Title: A+B - 8, Time: 184 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,19 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
  
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
  
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
  
-		int num = sc.nextInt();
- 
+		StringTokenizer st;
+        
 		for (int i = 1; i <= num; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
- 
-			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+			st = new StringTokenizer(br.readLine()," ");
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
 		}
- 
-		sc.close();
+		br.close();
 	}
+ 
 }
