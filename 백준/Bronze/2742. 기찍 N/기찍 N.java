1cc2366fd4b0b8df4032c459446b3e2dbba111a7
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-04 12:12:20 +0900

    [Bronze IV] Title: 기찍 N, Time: 216 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,21 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
-
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = Integer.parseInt(br.readLine());
-        br.close();
-        
-        for(int i=N;i>=1;i--){
-            bw.write(i+"\n");
-        }
-        bw.flush();
-        bw.close();
-    }
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+ 
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = N; i >= 1; i--) {
+			sb.append(i).append("\n");
+		}
+		System.out.print(sb);
+	}
 }
