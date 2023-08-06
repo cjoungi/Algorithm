1e041c37cf329a445156b1eae3dd8e353165b9ad
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-06 15:04:40 +0900

    [Bronze IV] Title: 상근날드, Time: 184 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/5543. 상근날드/상근날드.java
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -1,14 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         
         int hamMin = 2001;
         int bevMin = 2001;
         
         for(int i=1;i<=5;i++){
-            int price = sc.nextInt();
+            int price = Integer.parseInt(br.readLine());
             
             if(i <= 3){ // 햄버거
                 hamMin = Math.min(hamMin,price);
