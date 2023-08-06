efd1fa9058fbcf48eb92bcf1afb108051ae9e8f5
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-06 19:54:39 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 144 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        String S = br.readLine();
+        
+        int[] arr = new int[26]; // 알파벳의 개수를 저장하는 배열
+        
+        for(int i=0;i<S.length();i++){
+            int a = S.charAt(i) - 97;
+            arr[a]++;
+        }
+        
+        for(int j=0;j<26;j++){
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
