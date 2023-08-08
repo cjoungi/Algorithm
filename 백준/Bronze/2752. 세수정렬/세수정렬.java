a1af888f03f8057c2676c5d951ae8eaad86d7392
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-08 18:39:54 +0900

    [Bronze IV] Title: 세수정렬, Time: 144 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,18 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.Arrays;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int[] arr = new int[3];
-        
         for(int i=0;i<3;i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr); // 배열 정렬
+        Arrays.sort(arr);
         
-        for(int i=0;i<3;i++){
-            System.out.print(arr[i] + " ");
-        }
+        for(int num : arr) System.out.print(num + " ");
     }
 }
