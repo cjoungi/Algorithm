14b074c9aa198cecd1a3259fc00c9b4b81292c97
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-09 18:08:42 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 160 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -1,23 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int D = sc.nextInt();
- 
-        C += D % 60;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(br.readLine());
+        
+        // 1분은 60초이기 때문에 D초를 60으로 나눠서 나온 몫과 나머지를 각 분, 초에 합산
         B += D / 60;
+        C += D % 60;
         
+        // C초를 60초 이히로 정리해주기 위해 60으로 나눠서 몫과 나머지를 분과 초에 합산 및 대입
         B += C / 60;
         C = C % 60;
         
+        // B분을 60분 이하로 정리해주기 위해 60으로 나눠서 몫과 나머지를 분과 초에 합산 및 대입
         A += B / 60;
         B = B % 60;
         
+        // A시를 24시 이하로 정리해주기 위해 24로 나눠서 나머지만 대입
         A = A % 24;
         
         System.out.println(A + " " + B + " " + C);
