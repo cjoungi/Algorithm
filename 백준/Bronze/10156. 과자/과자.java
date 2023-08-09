6f072ff9900e3c0d94f147d16d0a6afba4541b78
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-09 20:01:32 +0900

    [Bronze IV] Title: 과자, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10156. 과자/과자.java
+++ 백준/Bronze/10156. 과자/과자.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
-        String[] arr = br.readLine().split(" ");
-        int K = Integer.parseInt(arr[0]); // 과자 1개 가격
-        int N = Integer.parseInt(arr[1]); // 과자 개수
-        int M = Integer.parseInt(arr[2]); // 내가 가진 돈
-   
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int K = Integer.parseInt(st.nextToken()); // 과자 1개 가격
+        int N = Integer.parseInt(st.nextToken()); // 과자 개수
+        int M = Integer.parseInt(st.nextToken()); 
+        
         if(K * N - M < 0) System.out.println(0);
         else System.out.println(K * N - M);
     }
