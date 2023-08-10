1159a4eb5c66224c6f9c310b275ccc10d86a72bf
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-10 10:14:42 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 116 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
         
-        if(a + b + c == 180){
-            if(a == 60 && b == 60 && c == 60){
-                System.out.println("Equilateral");
-            }else if(a == b || b == c || c == a){
-                System.out.println("Isosceles");
-            }else{
-                System.out.println("Scalene");
-            }
-        }else{
+        if(a + b + c != 180){ // 세 각의 합이 180이 아닌 경우
             System.out.println("Error");
+        }else if(a == 60 && b == 60 && c == 60){ // 세 각의 크기가 모두 60인 경우
+            System.out.println("Equilateral");
+        }else if(a == b || b == c || c == a){ // 세 각의 합이 180이고, 두 각이 같은 경우
+            System.out.println("Isosceles");
+        }else{ // 세 각의 합이 180이고, 같은 각이 없는 경우
+            System.out.println("Scalene");
         }
     }
 }
