19e09c9ca906db5566276b0beb7cd068d51cbebe
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-10 11:58:31 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         
@@ -22,9 +20,8 @@ public class Main{
         
         for(int i=0;i<5;i++){
             arr[i] = Integer.parseInt(st.nextToken()) - L * P; // 참가자 수 차이
-            bw.write(arr[i] + " ");
+            sb.append(arr[i]).append(" ");
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
