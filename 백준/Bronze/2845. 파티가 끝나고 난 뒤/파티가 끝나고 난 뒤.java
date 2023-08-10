b47735de45842a1089763c91ef69aa2c651690db
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-10 11:49:15 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 164 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int L = sc.nextInt(); // 1m²당 사람의 수
-        int P = sc.nextInt(); // 파티 장소 넓이
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int L = Integer.parseInt(st.nextToken()); // 1m²당 사람의 수
+        int P = Integer.parseInt(st.nextToken()); // 파티 장소 넓이
+        
+        st = new StringTokenizer(br.readLine());
         
         int[] arr = new int[5];
         String str = "";
         
         for(int i=0;i<5;i++){
-            arr[i] = sc.nextInt() - L * P;
+            arr[i] = Integer.parseInt(st.nextToken()) - L * P; // 참가자 수 차이
             str += arr[i] + " ";
         }
         System.out.println(str);
