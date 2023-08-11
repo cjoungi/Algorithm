8f1ffbc804e29d484f89e20401b3e6cbff8cc270
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-11 11:43:11 +0900

    [Bronze IV] Title: 방학 숙제, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/5532. 방학 숙제/방학 숙제.java
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -1,23 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         
-        int L = sc.nextInt(); // 방학 일 수
-        int A = sc.nextInt(); // 국어 총 페이지
-        int B = sc.nextInt(); // 수학 총 페이지
-        int C = sc.nextInt(); // 하루에 풀 수 있는 국어 페이지
-        int D = sc.nextInt(); // 하루에 풀 수 있는 수학 페이지
-        int dayA = 0, dayB = 0;
+        int L = Integer.parseInt(br.readLine()); // 방학 일 수
+        int A = Integer.parseInt(br.readLine()); // 국어 총 페이지
+        int B = Integer.parseInt(br.readLine()); // 수학 총 페이지
+        int C = Integer.parseInt(br.readLine()); // 하루에 풀 수 있는 국어 페이지
+        int D = Integer.parseInt(br.readLine()); // 하루에 풀 수 있는 수학 페이지
         
-        if(A % C != 0) dayA = A / C + 1;
-        else dayA = A / C;
-            
-        if(B % D != 0) dayB = B / D + 1;
-        else dayB = B / D;
-            
-        if(dayA >= dayB) System.out.println(L - dayA);
-        else System.out.println(L - dayB);
+        double max = Math.max((double)A / (double)C , (double)B / (double)D);
+        max = Math.ceil(max);
+        
+        System.out.println(L - (int)max);
     }
 }
