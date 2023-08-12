8cb843a688459fe327f10fcebde50d2e76dc2493
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-13 03:10:32 +0900

    [Bronze IV] Title: 모음의 개수, Time: 112 ms, Memory: 13932 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         while(true){
-            String str = sc.nextLine().toLowerCase(); // 소문자로 변환
+            String str = br.readLine().trim().toLowerCase(); // 소문자로 변환
             int count = 0;
             
             if(str.equals("#")){
