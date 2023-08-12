1cddd2183ff648e3a4e5f4c2041315343ba2cb7a
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-12 10:28:03 +0900

    [Bronze IV] Title: 시험 점수, Time: 116 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/5596. 시험 점수/시험 점수.java
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -1,20 +1,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] arrS = br.readLine().split(" ");
-        String[] arrT = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = 0, T = 0;
         
-        int S = 0,T = 0;
-        
-        for(int i=0;i<4;i++){
-            S += Integer.parseInt(arrS[i]);
-            T += Integer.parseInt(arrT[i]);
+        while(st.hasMoreTokens()){
+            S += Integer.parseInt(st.nextToken());
         }
+        
+        st = new StringTokenizer(br.readLine());
+        
+        while(st.hasMoreTokens()){
+            T += Integer.parseInt(st.nextToken());
+        }
+      
         System.out.println(Math.max(S,T));
     }
 }
