5796fec286b0be27472f6dcc43cd38a6b115e821
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-13 23:43:02 +0900

    [Bronze IV] Title: !밀비 급일, Time: 156 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         while(true){
-            String str = sc.nextLine();
+            String str = br.readLine();
             char[] c = new char[str.length()];
             
             if(str.equals("END")){
@@ -18,7 +20,7 @@ public class Main{
             for(int i=0;i<c.length;i++){
                 System.out.print(c[i]);
             }
-            System.out.print("\n");
+            System.out.println();
         }
     }
 }
