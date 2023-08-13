f0d4b8cb85b483d9866fd7e116cf363562593ce9
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-13 23:54:33 +0900

    [Bronze IV] Title: !밀비 급일, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -1,32 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
-         while(true){
+        while(true){
             String str = br.readLine();
-            char[] c = new char[str.length()];
             
             if(str.equals("END")){
                 break;
             }
             
-            for(int i=0;i<c.length;i++){
-                c[c.length - 1 - i] = str.charAt(i);
-            }
-            for(int i=0;i<c.length;i++){
-                bw.write(c[i]);
-            }
-            bw.write("\n");
+            StringBuilder r = new StringBuilder(str);
+            sb.append(r.reverse()).append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
