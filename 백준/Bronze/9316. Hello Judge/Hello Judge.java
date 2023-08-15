c54b9f8217416becf69e6fb8ff3842742b4fd753
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-16 01:49:28 +0900

    [Bronze IV] Title: Hello Judge, Time: 136 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/9316. Hello Judge/Hello Judge.java
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -1,16 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine());
 
         for(int i=1;i<=N;i++){
-            System.out.println("Hello World, Judge " + i + "!");
+            bw.write("Hello World, Judge " + i + "!" + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
