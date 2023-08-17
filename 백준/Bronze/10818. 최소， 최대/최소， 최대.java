47196f21af514c706aa07b69882c6564d27a8f66
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-17 20:32:12 +0900

    [Bronze III] Title: 최소, 최대, Time: 1428 ms, Memory: 110044 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int[] arr = new int[N];
         
         for(int i=0;i<N;i++){
-          arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         System.out.println(arr[0] + " " + arr[N-1]);
