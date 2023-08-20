8735b2e63fd3178fcf2bfd93c65ec5fdb45d9a21
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-21 01:06:44 +0900

    [Bronze III] Title: ACM 호텔, Time: 280 ms, Memory: 19084 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt(); // 테스트 데이터 수
+        
+        for(int i=0;i<T;i++){
+            int H = sc.nextInt(); // 호텔의 층 수
+            int W = sc.nextInt(); // 각 층의 방 수
+            int N = sc.nextInt(); // N번째 손님
+            
+            if(N % H == 0){ // N이 H의 배수일 때
+                if(N / H < 10) System.out.println(H + "0" + N / H);
+                else System.out.println(H + "" + N / H);
+            }else{
+                if(N / H + 1 < 10) System.out.println(N % H + "0" + (N / H + 1));
+                else System.out.println(N % H + "" + (N / H + 1));
+            }
+        }
+    }
+}
