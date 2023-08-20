acee469ee9b4ec35bd2e187c1ba2f5ba04f46542
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-21 02:20:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 136 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int T = sc.nextInt(); // 테스트 데이터 수
+        int T = Integer.parseInt(br.readLine()); // 테스트 데이터 수
         
         for(int i=0;i<T;i++){
-            int H = sc.nextInt(); // 호텔의 층 수
-            int W = sc.nextInt(); // 각 층의 방 수
-            int N = sc.nextInt(); // N번째 손님
+            StringTokenizer st = new StringTokenizer(br.readLine());
             
-            if(N % H == 0){ // N이 H의 배수일 때
-                if(N / H < 10) System.out.println(H + "0" + N / H);
-                else System.out.println(H + "" + N / H);
+            int H = Integer.parseInt(st.nextToken()); // 호텔의 층 수
+            int W = Integer.parseInt(st.nextToken()); // 각 층의 방 수
+            int N = Integer.parseInt(st.nextToken()); // N번째 손님
+            
+            if(N % H == 0){
+                System.out.println(H * 100 + N / H);
             }else{
-                if(N / H + 1 < 10) System.out.println(N % H + "0" + (N / H + 1));
-                else System.out.println(N % H + "" + (N / H + 1));
+                System.out.println(N % H * 100 + (N / H + 1));
             }
         }
     }
