944f8410e6f87301fd170dcb813bce7cc38e61c1
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-23 08:43:31 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         while(true){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
             
-            // 0 0 0 을 입력받으면 종료
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
             if(a==0 && b==0 && c==0) break;
             
             if(Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2)){
@@ -22,5 +26,6 @@ public class Main{
                 System.out.println("wrong");
             }
         }
+            
     }
 }
