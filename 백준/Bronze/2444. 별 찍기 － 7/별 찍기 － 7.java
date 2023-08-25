f9f7866f118ae6f182cd9c996d0ca3d7e178529c
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-08-25 09:21:58 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 144 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,31 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine()); 
 
         for(int i=0;i<N;i++){
             for(int j=1;j<N-i;j++){
-                System.out.print(" ");
+                bw.write(" ");
             }
             for(int j=0;j<2*i+1;j++){
-                System.out.print("*");
+                bw.write("*");
             }
-            System.out.println();
+            bw.write("\n");
         }
         
         for(int i=N-1;i>0;i--){
             for(int j=0;j<N-i;j++){
-                System.out.print(" ");
+                bw.write(" ");
             }
             for(int j=0;j<2*i-1;j++){
-                System.out.print("*");
+                bw.write("*");
             }
-            System.out.println();
+            bw.write("\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
