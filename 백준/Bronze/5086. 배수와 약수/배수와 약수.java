3eabadae0cb8914817a99deee6d3e2226231dd81
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-09-01 23:35:42 +0900

    [Bronze III] Title: 배수와 약수, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         while(true){
-            int a = sc.nextInt(); 
-            int b = sc.nextInt(); 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             
             if(a==0 && b==0){
                 break;
@@ -19,7 +24,6 @@ public class Main{
             }else{
                 System.out.println("neither");
             }
-            
         }
     }
 }
