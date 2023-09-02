3e269a596409d7d50fde47d329b1f8a05d21fc3a
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-09-02 20:31:48 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 136 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -1,26 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine()); 
 
         for(int i=0;i<N;i++){
             for(int j=0;j<i;j++){
-                bw.write(" ");
+                sb.append(" ");
             }
             for(int j=0;j<2*(N-i)-1;j++){
-                bw.write("*");
+                sb.append("*");
             }
-            bw.write("\n");
+            sb.append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
