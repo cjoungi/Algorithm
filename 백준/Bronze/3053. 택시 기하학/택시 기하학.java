65c796f6c0a1197ea0210a9657f6e422d260f968
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-09-06 15:37:06 +0900

    [Bronze III] Title: 택시 기하학, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/3053. 택시 기하학/택시 기하학.java
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         
-        int r = sc.nextInt();
-        
+        int r = Integer.parseInt(br.readLine());
+
         System.out.println(Math.PI*r*r);
         System.out.println(2*r*r);
     }
