eba1e47e38b1bfa150b64bf3213a5ba3d47001c4
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-09-07 00:24:59 +0900

    [Bronze III] Title: 공 넣기, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,20 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
- 
-public class Main {
-	public static void main(String args[]) throws IOException {
- 
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         
-		int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
 		int[] arr = new int[N];
 		
-		for(int a=1;a<=M;a++) {
+		for(int a=0;a<M;a++) {
             st = new StringTokenizer(br.readLine());
             
 			int i = Integer.parseInt(st.nextToken());
@@ -26,9 +26,8 @@ public class Main {
 			}
 		}
 		for(int num : arr) {
-			System.out.print(num + " ");
+			sb.append(num).append(" ");
 		}
-		br.close();
-	}
- 
+        System.out.println(sb);
+    }
 }
