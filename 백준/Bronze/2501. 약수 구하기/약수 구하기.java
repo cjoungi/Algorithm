799b17291576854023b2fad297f2ab9c7ab1c3f2
JoungIm Cho <113075984+cjoungi@users.noreply.github.com>
2023-09-10 23:18:12 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         int num = 0;
         
         for(int i=1;i<=N;i++) {
@@ -14,6 +18,7 @@ public class Main{
         		
         		if(num == K) {
         			System.out.println(i);
+                    break;
         		}
         	}
         }
